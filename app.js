dedc10e2d189e119d31d12d1968671a722597b06
Nic Nash <nicnash08@gmail.com>
2014-09-25 20:48:32 -0500

    too many folders named portfolio jeeeze

--- app.js
+++ app.js
@@ -3,5 +3,48 @@ var App = Ember.Application.create({
 });
 
 App.Router.map(function(){
-	this.route('about', {path: '/aboutus'});
+    this.route('index', {path: '/index'});
+	this.route('about', {path: '/about'});
+    this.resource('project', function(){
+                        this.resource('project',{path: '/:title'});
+                    });
 });
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.ProjectsController = Ember.Controller.extend({
+
+});
+
+App.PROJECTS = DS.Model.extend({
+                title:DS.attr('string'),
+                description:DS.attr('string'),
+                languages:DS.attr('string')
+        });
+
+App.PROJECTS.FIXTURES = [
+
+    {
+        title:'jumbo bsns',
+        description:'displays all jumbo things',
+        languages:'dance aroudn guys!'
+    },
+    {
+        title:'aimp',
+        description:'asset info platform',
+        languages:'dance aroudn guys!'
+    },
+    {
+        title:'tutor r us',
+        description:'jfee',
+        languages:'fortran!'
+    }
+
+];
+
+App.ProjectRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.PROJECTS.findBy('title', params.title);
+  }
+});
+ 
